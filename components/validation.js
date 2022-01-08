56024f4a80a817777c8c6356adc2a69ab9de4cc8
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-01-08 22:38:31 +0700

    форма для отправки сообщения + валидация

---
+++ components/validation.js
@@ -0,0 +1,82 @@
+export default class FormValidator {
+  constructor(form) {
+    this._inputSelector = ".form-control";
+    this._submitButtonSelector = ".form-submit";
+    this._inputErrorClass = "is-invalid";
+    this._inputAccessClass = "is-valid";
+    this._errorClass = "invalid-feedback";
+    this._accessClass = "valid-feedback";
+    this._form = form;
+    this._inputList = Array.from(this._form.querySelectorAll(this._inputSelector)).slice(0, 2);
+    this._submitButton = this._form.querySelector(this._submitButtonSelector);
+  }
+
+  setDefaultForm() {
+    this._toggleButton();
+    this._inputList.forEach((input) => {
+      this._showInputSuccess(input);
+    });
+  }
+
+  _showInputError(input, errorMessage) {
+    const errorElement = this._form.querySelector(`#${input.name}`);
+    input.classList.add(this._inputErrorClass);
+    input.classList.remove(this._inputAccessClass);
+    errorElement.textContent = errorMessage;
+  }
+
+  _hideInputMessage(input) {
+    const errorElement = this._form.querySelector(`#${input.name}`);
+    input.classList.remove(this._inputErrorClass);
+    input.classList.remove(this._inputAccessClass);
+    errorElement.textContent = "";
+  }
+
+  _showInputSuccess(input) {
+    const errorElement = this._form.querySelector(`#${input.name}`);
+    input.classList.remove(this._inputErrorClass);
+    input.classList.add(this._inputAccessClass);
+    errorElement.textContent = "";
+  }
+
+  _checkInputValidity = (input) => {
+    if (!input.value) {
+      this._hideInputMessage(input);
+      return;
+    }
+    if (!input.validity.valid) {
+      this._showInputError(input, input.validationMessage);
+    } else {
+      this._showInputSuccess(input);
+    }
+  };
+
+  _hasInvalidInputs() {
+    return this._inputList.some((input) => !input.validity.valid);
+  }
+
+  _toggleButton = () => {
+    if (this._hasInvalidInputs()) {
+      this._submitButton.disabled = true;
+    } else {
+      this._submitButton.disabled = false;
+    }
+  };
+
+  _setEventListeners() {
+    this._formSubmit;
+    this._inputList.forEach((input) =>
+      input.addEventListener("input", () => {
+        this._toggleButton();
+        this._checkInputValidity(input);
+      })
+    );
+  }
+
+  enableValidation() {
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import { age, declination } from "../utils/calcAge.js";
 import modal from "../components/modal.js";
+import FormValidation from "../components/validation.js";
 
+const form = document.querySelector(".form");
+
+const fromValidation = new FormValidation(form);
+fromValidation.enableValidation();
 // возраст
 document.querySelector(".age").textContent = age + " " + declination;
 
