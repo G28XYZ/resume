530a2a7dd05e24bb335863fcdc3fe3e73826db0a
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-01-08 15:29:30 +0700

    блок портфолио

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,37 @@ import { age, declination } from "../utils/calcAge.js";
 // возраст
 document.querySelector(".age").textContent = age + " " + declination;
 
-var triggerTabList = [].slice.call(document.querySelectorAll("#my-nav a"));
-triggerTabList.forEach(function (triggerEl) {
-  var tabTrigger = new bootstrap.Tab(triggerEl);
+const span = '<span class="visually-hidden">(current)</span>';
 
+function toggleNavItem() {
+  triggerTabList.forEach((item) => item.classList.remove("active"));
+}
+
+const triggerTabList = [].slice.call(document.querySelectorAll("#my-nav a")),
+  animationTime = 500,
+  framesCount = 20;
+
+triggerTabList.forEach(function (triggerEl) {
   triggerEl.addEventListener("click", function (e) {
+    toggleNavItem();
     e.preventDefault();
-    tabTrigger.show();
+    e.target.classList.add("active");
+    let coordY =
+      document.querySelector(triggerEl.getAttribute("href")).getBoundingClientRect().top +
+      window.pageYOffset;
+
+    let scroller = setInterval(function () {
+      let scrollBy = coordY / framesCount;
+
+      if (
+        scrollBy > window.pageYOffset - coordY &&
+        window.innerHeight + window.pageYOffset < document.body.offsetHeight
+      ) {
+        window.scrollBy(0, scrollBy);
+      } else {
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    }, animationTime / framesCount);
   });
 });
