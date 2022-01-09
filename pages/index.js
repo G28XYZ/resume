cf8802a41fd12058d9abd61602803bb84c77267f
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-01-09 11:37:34 +0700

    убрал скролл по странице

--- pages/index.js
+++ pages/index.js
@@ -12,31 +12,13 @@ function toggleNavItem() {
   triggerTabList.forEach((item) => item.classList.remove("active"));
 }
 
-const triggerTabList = [].slice.call(document.querySelectorAll("#my-nav a")),
-  animationTime = 500,
-  framesCount = 20;
-
 triggerTabList.forEach(function (triggerEl) {
   triggerEl.addEventListener("click", function (e) {
     toggleNavItem();
     e.preventDefault();
     e.target.classList.add("active");
-    let coordY =
-      document.querySelector(triggerEl.getAttribute("href")).getBoundingClientRect().top +
-      window.pageYOffset;
-
-    let scroller = setInterval(function () {
-      let scrollBy = coordY / framesCount;
-
-      if (
-        scrollBy > window.pageYOffset - coordY &&
-        window.innerHeight + window.pageYOffset < document.body.offsetHeight
-      ) {
-        window.scrollBy(0, scrollBy);
-      } else {
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-    }, animationTime / framesCount);
+    window.scrollBy({
+      behavior: "smooth",
+    });
   });
 });
